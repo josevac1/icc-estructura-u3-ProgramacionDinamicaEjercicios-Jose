85372535e3421d857d63bc1abce079adf7a612b7
jose vanegas <josevanp05@gmail.com>
2024-07-15 17:53:14 -0500

    Trabajo Practica

--- src/Ejercicio/EjercicioDos.java
+++ src/Ejercicio/EjercicioDos.java
@@ -1,5 +1,8 @@
 package Ejercicio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Dados n pares de paréntesis, implementa un algoritmo para generar todas las combinaciones
  * válidas de paréntesis.
@@ -20,8 +23,42 @@ package Ejercicio;
  * Se debe usar StringBuilder
  */
 public class EjercicioDos {
+    public List<String> generateParenthesis(int n) {
+        // Inicializa la lista que contendrá todas las combinaciones de paréntesis
+        List<String> result = new ArrayList<>();
+        // Llama al método recursivo para generar las combinaciones
+        generate(result, new StringBuilder(), 0, 0, n);
+        // Devuelve la lista de combinaciones de paréntesis
+        return result;
+    }
 
-    // List<List<Integer>> subsets(List<Integer> set) {
+    // Método recursivo para generar combinaciones de paréntesis
+    private void generate(List<String> result, StringBuilder current, int open, int close, int max) {
+        // Si la longitud de la cadena actual es igual a max * 2, se ha generado una combinación válida
+        if (current.length() == max * 2) {
+            // Añade la combinación generada a la lista de resultados
+            result.add(current.toString());
+            return;
+        }
 
-    // }
+        // Si el número de paréntesis abiertos es menor que max, añade un paréntesis abierto
+        if (open < max) {
+            // Añade un paréntesis abierto a la cadena actual
+            current.append('(');
+            // Llama recursivamente con un paréntesis abierto adicional
+            generate(result, current, open + 1, close, max);
+            // Elimina el último carácter añadido (retroceso)
+            current.deleteCharAt(current.length() - 1);
+        }
+        // Si el número de paréntesis cerrados es menor que el número de abiertos, añade un paréntesis cerrado
+        if (close < open) {
+            // Añade un paréntesis cerrado a la cadena actual
+            current.append(')');
+            // Llama recursivamente con un paréntesis cerrado adicional
+            generate(result, current, open, close + 1, max);
+            // Elimina el último carácter añadido (retroceso)
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+
 }
--- src/Ejercicio/EjercicioUno.java
+++ src/Ejercicio/EjercicioUno.java
@@ -1,7 +1,9 @@
 package Ejercicio;
 
 import java.util.ArrayList;
+//import java.util.HashMap;
 import java.util.List;
+//import java.util.Map;
 
 /*
  * Implementa un método para devolver todos los subconjuntos de un arreglo de enteros
@@ -28,8 +30,44 @@ import java.util.List;
  * Ecoja la mejor estructura de datos para que sea mas eficiente
  */
 public class EjercicioUno {
+    
+    public List<List<Integer>> subsets(List<Integer> set){
+        List<List<Integer>> subsets=new ArrayList<List<Integer>>();
+        subsets.add(new ArrayList<Integer>());
 
-    // List<List<Integer>> subsets(List<Integer> set) {
+        for (Integer element :set ){
+            addSetByElement(element, subsets);
+        }
 
-    // }
+        return subsets;
+    }
+    private void addSetByElement(Integer element,List<List<Integer>> subsets){
+        int currentSize =subsets.size();
+        for(int i =0; i< currentSize;i++){
+            List<Integer> newSubset=new ArrayList<Integer>(subsets.get(i));
+            newSubset.add(element);
+            subsets.add(newSubset);
+        }
+    }
+
+    //public List<List<Integer>> subsets(List<Integer> set) {
+    //    List<List<Integer>> subsets = new ArrayList<>();
+    //    Map<Integer, List<Integer>> map = new HashMap<>();
+    //    
+    //    int n = set.size();
+    //    int numOfSubsets = 1 << n; // 2^n subsets
+    //    
+    //    for (int i = 0; i < numOfSubsets; i++) {
+    //        List<Integer> subset = new ArrayList<>();
+    //        for (int j = 0; j < n; j++) {
+    //            if ((i & (1 << j)) != 0) {
+    //                subset.add(set.get(j));
+    //            }
+    //        }
+    //        subsets.add(subset);
+    //        map.put(i, subset);
+    //    }
+    //    
+    //    return subsets;
+    //}
 }
